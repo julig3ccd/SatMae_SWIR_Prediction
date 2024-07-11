3828f25bb992e1e9daf2013beda8e6c4b503641a
Julian Geißinger <122826907+julig3ccd@users.noreply.github.com>
2024-07-11 12:49:59 +0200

    Update validate_only.py

--- validate_only.py
+++ validate_only.py
@@ -301,9 +301,7 @@ def main(args):
 
 if __name__ == '__main__':
     args = get_args_parser()
-    print("got the args parser: " ,args)
     args = args.parse_args()
-    print("after parse args " ,args)
     if args.output_dir:
         Path(args.output_dir).mkdir(parents=True, exist_ok=True)
     main(args)

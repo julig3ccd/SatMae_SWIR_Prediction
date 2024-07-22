2997b7af1370678f3357334ff661c4a8a4900980
Julian Geißinger <122826907+julig3ccd@users.noreply.github.com>
2024-07-22 23:19:07 +0200

    wanb init update

--- validate_only.py
+++ validate_only.py
@@ -230,7 +230,7 @@ def main(args):
 
     # Set up wandb
     if global_rank == 0 and args.wandb is not None:
-        wandb.init(project=args.wandb, entity="julian_geissinger")
+        wandb.init(project=args.wandb, entity="JULIAN_GEISSINGER")
         wandb.config.update(args)
         wandb.watch(model)
 

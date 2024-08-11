ed1696162cda933f1b58c5e77a35bfdd197c7a68
Julian Geißinger <122826907+julig3ccd@users.noreply.github.com>
2024-08-11 15:26:46 +0200

    only try to remove keys from checkpoint if they exist

--- util/misc.py
+++ util/misc.py
@@ -321,8 +321,11 @@ def load_model(args, model_without_ddp, optimizer, loss_scaler):
                 args.resume, map_location='cpu', check_hash=True)
         else:
             checkpoint = torch.load(args.resume, map_location='cpu')
-            del checkpoint['model']['head.weight']
-            del checkpoint['model']['head.bias']
+            #only delete keys if the exist on checkpoint
+            for key in ['head.weight', 'head.bias']:
+                if key in checkpoint['model']:
+                    del checkpoint['model'][key]
+    
         model_without_ddp.load_state_dict(checkpoint['model'], strict=False)
         print("Resume checkpoint %s" % args.resume)
         if 'optimizer' in checkpoint and 'epoch' in checkpoint and not (hasattr(args, 'eval') and args.eval):

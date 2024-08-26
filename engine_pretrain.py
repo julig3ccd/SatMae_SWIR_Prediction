24f395720c8f91a5254a11c14f1ee04acab52966
Julian Geißinger <122826907+julig3ccd@users.noreply.github.com>
2024-08-26 13:43:48 +0200

    fix argument parsing error

--- engine_pretrain.py
+++ engine_pretrain.py
@@ -112,7 +112,7 @@ def train_one_epoch(model: torch.nn.Module,
         #print("SAMPLES / MODEL INPUT SHAPE: ", samples.shape) --> [16, 10, 96, 96]
         #print("FIRST SAMPLE SHAPE : ", samples[0].shape) --> ([10, 96, 96])
         with torch.cuda.amp.autocast():
-            loss, pred, mask = model(samples, targets, mask_ratio=args.mask_ratio)
+            loss, pred, mask = model(samples, targets=targets, mask_ratio=args.mask_ratio)
             #print("PRED SHAPE: ", pred.shape) --> ([16, 10, 144, 64]) [Batch, Channels, SeqLen, p^2]
             #print("MASK SHAPE: ", mask.shape) --> ([16, 3, 144])
 

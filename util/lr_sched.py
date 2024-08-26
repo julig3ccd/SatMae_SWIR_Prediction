a4457222987bca1cd23f93f27b36c81ede76844f
Julian Geißinger <122826907+julig3ccd@users.noreply.github.com>
2024-08-26 14:37:54 +0200

    debug 0 learning rate

--- util/lr_sched.py
+++ util/lr_sched.py
@@ -12,9 +12,13 @@ def adjust_learning_rate(optimizer, epoch, args):
     else:
         lr = args.min_lr + (args.lr - args.min_lr) * 0.5 * \
             (1. + math.cos(math.pi * (epoch - args.warmup_epochs) / (args.epochs - args.warmup_epochs)))
+    print("CALCULATED LEARNING RATE: ", lr)
     for param_group in optimizer.param_groups:
         if "lr_scale" in param_group:
             param_group["lr"] = lr * param_group["lr_scale"]
         else:
             param_group["lr"] = lr
+    
+    print(" LEARNING RATE after adjustment: ", lr)
+        
     return lr

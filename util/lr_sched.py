3c0e7222513f8772667c2ee09c31571a40eafa07
Julian Geißinger <122826907+julig3ccd@users.noreply.github.com>
2024-08-26 13:41:17 +0200

    fix format

--- util/lr_sched.py
+++ util/lr_sched.py
@@ -5,8 +5,7 @@
 
 import math
 
-def adjust_lea
-rning_rate(optimizer, epoch, args):
+def adjust_learning_rate(optimizer, epoch, args):
     """Decay the learning rate with half-cycle cosine after warmup"""
     if epoch < args.warmup_epochs:
         lr = args.lr * epoch / args.warmup_epochs 

79349726db1d36ed0847de75a5792de42a35ddb9
Julian Geißinger <122826907+julig3ccd@users.noreply.github.com>
2024-08-29 18:16:12 +0200

    SC-70: rm learning rate logs since it behaves as expected

--- util/lr_sched.py
+++ util/lr_sched.py
@@ -7,20 +7,20 @@ import math
 
 def adjust_learning_rate(optimizer, epoch, args):
     """Decay the learning rate with half-cycle cosine after warmup"""
-    print("EPOCH: ", epoch)
-    print ("WARMUP EPOCHS: ", args.warmup_epochs)
+    #print("EPOCH: ", epoch)
+    #print ("WARMUP EPOCHS: ", args.warmup_epochs)
     if epoch < args.warmup_epochs:
         lr = args.lr * epoch / args.warmup_epochs 
     else:
         lr = args.min_lr + (args.lr - args.min_lr) * 0.5 * \
             (1. + math.cos(math.pi * (epoch - args.warmup_epochs) / (args.epochs - args.warmup_epochs)))
-    print("CALCULATED LEARNING RATE: ", lr)
+    #print("CALCULATED LEARNING RATE: ", lr)
     for param_group in optimizer.param_groups:
         if "lr_scale" in param_group:
             param_group["lr"] = lr * param_group["lr_scale"]
         else:
             param_group["lr"] = lr
     
-    print(" LEARNING RATE after adjustment: ", lr)
+    #print(" LEARNING RATE after adjustment: ", lr)
         
     return lr

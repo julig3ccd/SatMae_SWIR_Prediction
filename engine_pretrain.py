15ddfcb66d72defda393e1395d41cc7c6f0f3c94
Julian Geißinger <122826907+julig3ccd@users.noreply.github.com>
2024-09-04 14:06:42 +0200

    fix argument error #2

--- engine_pretrain.py
+++ engine_pretrain.py
@@ -75,11 +75,11 @@ def evaluate(data_loader, model, device, args=None):
         
         if args.print_comparison:
               if idx % 100 == 0:
-                save_comparison_fig_from_tensor(swirpred,f'eval_comparison_fig_b_{idx}',target_images=swir_targets,num_channels=2,mask=mask,input=images)
+                save_comparison_fig_from_tensor(swirpred,f'eval_comparison_fig_b_{idx}',target_images=swir_targets,mask=mask,input=images)
                 print('saved swir comparison figures for batch ',idx)
         if args.print_input: 
             if idx % 100 == 0:
-                save_input_output_fig(final_swir_images=swirpred, name=f'input_output_fig_b_{idx}', target_images=swir_targets, num_channels=10, mask=mask, input=images)  
+                save_input_output_fig(final_swir_images=swirpred, name=f'input_output_fig_b_{idx}', target_images=swir_targets, mask=mask, input=images)  
                 print('saved input output figures for batch ',idx)   
         # acc1, acc5 = accuracy(output, target, topk=(1, 5))
         # print(acc1, acc5, flush=True)

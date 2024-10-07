345df15451212b7c1f8e3825e64fb3d32cdc2106
Julian Geißinger <122826907+julig3ccd@users.noreply.github.com>
2024-10-07 14:36:22 +0200

    add choice for unet

--- own_main_pretrain.py
+++ own_main_pretrain.py
@@ -44,7 +44,7 @@ def get_args_parser():
                         help='Accumulate gradient iterations (for increasing the effective batch size under memory constraints)')
 
     # Model parameters
-    parser.add_argument('--model_type', default=None, choices=['group_c', 'temporal', 'vanilla'],
+    parser.add_argument('--model_type', default=None, choices=['group_c', 'temporal', 'vanilla','shallow_unet'],
                         help='Use channel model')
     parser.add_argument('--model', default='mae_vit_large_patch16', type=str, metavar='MODEL',
                         help='Name of model to train')

ac755eb12f3bca03f4907d52d15bdb7313a09982
Julian Geißinger <122826907+julig3ccd@users.noreply.github.com>
2024-10-08 16:37:27 +0200

    fix printing UNET output

--- engine_pretrain.py
+++ engine_pretrain.py
@@ -157,11 +157,11 @@ def evaluateUNET(data_loader, model, device, args=None):
         
         if args.print_comparison:
               if idx % 100 == 0:
-                save_swir_comparison_fig_from_tensor(swirpred,f'eval_comparison_fig_b_{idx}_p_{args.patch_size}',target_images=swir_targets,input=images)
+                save_swir_comparison_fig_from_tensor(pred,f'eval_comparison_fig_b_{idx}_p_{args.patch_size}',target_images=swir_targets,input=images)
                 print('saved swir comparison figures for batch ',idx)
         if args.print_input: 
             if idx % 100 == 0:
-                save_input_output_fig(final_swir_images=swirpred, name=f'input_output_fig_b_{idx}_p_{args.patch_size}', target_images=swir_targets, input=images)  
+                save_input_output_fig(final_swir_images=pred, name=f'input_output_fig_b_{idx}_p_{args.patch_size}', target_images=swir_targets, input=images)  
                 print('saved input output figures for batch ',idx)   
     
 
